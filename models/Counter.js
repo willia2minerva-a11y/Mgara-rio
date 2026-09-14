07d49b3e500b49645451a6375f53e09e67e7c01e
William minerva <willia2minerva@gmail.com>
2026-09-14 14:44:50 +0100

    Create Counter model with mongoose schema

---
+++ models/Counter.js
@@ -0,0 +1,9 @@
+// models/Counter.js
+import mongoose from 'mongoose';
+
+const counterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 }
+});
+
+export default mongoose.model('Counter', counterSchema);

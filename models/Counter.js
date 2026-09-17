8dd94584bff12b2c17689e9ba50194ca8b738cfc
William minerva <willia2minerva@gmail.com>
2026-09-17 17:06:14 +0100

    Update Counter.js

--- models/Counter.js
+++ models/Counter.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 
 const counterSchema = new mongoose.Schema({
   _id: { type: String, required: true },
-  seq: { type: Number, default: 0 }
+  letter: { type: String, default: 'R' },  // الحرف الحالي
+  seq: { type: Number, default: 0 }         // آخر رقم
 });
 
 export default mongoose.model('Counter', counterSchema);
